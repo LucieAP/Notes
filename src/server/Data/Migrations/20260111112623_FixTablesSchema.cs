0aeac21e90da2756d9e2028cf877946790bf5b89
LucieAP <toshaletvinov1@gmail.com>
2026-01-11 15:11:29 +0300

    chore: add new database migration

---
+++ src/server/Data/Migrations/20260111112623_FixTablesSchema.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTablesSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NoteGroups_Users_UserId",
+                table: "NoteGroups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Users_UserId",
+                table: "Notes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Users_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskGroups_Users_UserId",
+                table: "TaskGroups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_UserId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_UserId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskGroups_UserId",
+                table: "TaskGroups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notes_UserId",
+                table: "Notes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_NoteGroups_UserId",
+                table: "NoteGroups");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TaskGroups");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "NoteGroups");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_CreatedBy",
+                table: "Tasks",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskGroups_CreatedBy",
+                table: "TaskGroups",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_CreatedBy",
+                table: "Recipes",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_CreatedBy",
+                table: "Notes",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoteGroups_CreatedBy",
+                table: "NoteGroups",
+                column: "CreatedBy");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NoteGroups_Users_CreatedBy",
+                table: "NoteGroups",
+                column: "CreatedBy",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Users_CreatedBy",
+                table: "Notes",
+                column: "CreatedBy",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Users_CreatedBy",
+                table: "Recipes",
+                column: "CreatedBy",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskGroups_Users_CreatedBy",
+                table: "TaskGroups",
+                column: "CreatedBy",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_CreatedBy",
+                table: "Tasks",
+                column: "CreatedBy",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_NoteGroups_Users_CreatedBy",
+                table: "NoteGroups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Users_CreatedBy",
+                table: "Notes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_Users_CreatedBy",
+                table: "Recipes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskGroups_Users_CreatedBy",
+                table: "TaskGroups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_CreatedBy",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_CreatedBy",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskGroups_CreatedBy",
+                table: "TaskGroups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_CreatedBy",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notes_CreatedBy",
+                table: "Notes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_NoteGroups_CreatedBy",
+                table: "NoteGroups");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Tasks",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "TaskGroups",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Recipes",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Notes",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "NoteGroups",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_UserId",
+                table: "Tasks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskGroups_UserId",
+                table: "TaskGroups",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_UserId",
+                table: "Recipes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_UserId",
+                table: "Notes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoteGroups_UserId",
+                table: "NoteGroups",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_NoteGroups_Users_UserId",
+                table: "NoteGroups",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Users_UserId",
+                table: "Notes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_Users_UserId",
+                table: "Recipes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskGroups_Users_UserId",
+                table: "TaskGroups",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_UserId",
+                table: "Tasks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
