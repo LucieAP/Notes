1c59a9343be7f5934010100db0b32bd45d97bacc
LucieAP <toshaletvinov1@gmail.com>
2026-01-15 23:45:24 +0300

    Add GoogleUserInfo dto to store data from google response

---
+++ src/server/Models/DTOs/Auth/GoogleUserInfo.cs
@@ -0,0 +1,8 @@
+public class GoogleUserInfo
+{
+    public string GoogleId { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public bool EmailVerified { get; set; }
+    public string Picture { get; set; }
+}
