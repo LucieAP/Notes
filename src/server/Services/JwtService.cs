0c1d51c24ee6df5adde890b400777774cd4a05a9
LucieAP <toshaletvinov1@gmail.com>
2026-01-15 23:32:32 +0300

    Add JwtService
    - Add jwt token generation method

---
+++ src/server/Services/JwtService.cs
@@ -0,0 +1,30 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+public class JwtService
+{
+    public string GenerateJwtToken(JwtUser user)
+    {
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Name),
+            new Claim(ClaimTypes.Email, user.Email),
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JWT")));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: Environment.GetEnvironmentVariable("JWT_ISSUER"),
+            audience: Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(30),  // Время жизни токена
+            signingCredentials: creds);
+        
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
