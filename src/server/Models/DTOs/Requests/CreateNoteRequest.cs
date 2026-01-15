a28bac47017dfe26629b833fee968c2b8911c042
LucieAP <toshaletvinov1@gmail.com>
2026-01-15 23:40:27 +0300

    Add CreateNoteRequest dto

---
+++ src/server/Models/DTOs/Requests/CreateNoteRequest.cs
@@ -0,0 +1,10 @@
+public class CreateNoteRequest
+{
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public bool IsPinned { get; set; }
+
+    public BackgroundColor BackgroundColor { get; set; } = BackgroundColor.Default;
+
+    public Guid? NoteGroupId  {get; set;}
+}
