50ac8d15464cbc742a4af9a10d8cc7f209c93047
LucieAP <toshaletvinov1@gmail.com>
2026-01-18 21:33:36 +0300

    Add new migration with renamed IsDeleted property

---
+++ src/server/Data/Migrations/20260118183210_RenameNoteGroupProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameNoteGroupProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isDeleted",
+                table: "NoteGroups",
+                newName: "IsDeleted");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "NoteGroups",
+                newName: "isDeleted");
+        }
+    }
+}
