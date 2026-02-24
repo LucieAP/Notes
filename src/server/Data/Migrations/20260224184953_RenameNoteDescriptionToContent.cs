597f2d6ca9abc55e1802f56ba36b3be2bcdfa8e1
LucieAP <toshaletvinov1@gmail.com>
2026-02-24 21:50:44 +0300

    Add new migration

---
+++ src/server/Data/Migrations/20260224184953_RenameNoteDescriptionToContent.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameNoteDescriptionToContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Notes");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Tasks",
+                newName: "Content");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Recipes",
+                newName: "Content");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Notes",
+                type: "character varying(65535)",
+                maxLength: 65535,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Notes");
+
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Tasks",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Recipes",
+                newName: "Description");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Notes",
+                type: "character varying(5000)",
+                maxLength: 5000,
+                nullable: true);
+        }
+    }
+}
