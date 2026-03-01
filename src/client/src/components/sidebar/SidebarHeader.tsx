75d93638190e5b9d6e28d14d01f11ce2b12672e7
LucieAP <toshaletvinov1@gmail.com>
2026-03-01 14:49:22 +0300

    Add default avatar for user

--- src/client/src/components/sidebar/SidebarHeader.tsx
+++ src/client/src/components/sidebar/SidebarHeader.tsx
@@ -1,6 +1,7 @@
 import useAuth from "@/shared/hooks/useAuth";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../common/buttons/LogoutButton";
+import defaultAvatar from "@/components/common/icons/default_avatar.png";
 
 function SidebarHeader() {
   const { user, logout } = useAuth();
@@ -12,11 +13,19 @@ function SidebarHeader() {
         className="flex items-center gap-2 px-2 py-1 rounded-lg cursor-pointer transition-colors select-none"
       >
         <div className="rounded-full bg-neutral-700 flex items-center justify-center text-white text-xs font-medium">
-          <img
-            className="w-5 h-5 rounded-full"
-            src={user?.picture?.toString()}
-            alt=""
-          />
+          {user?.picture ? (
+            <img
+              className="w-5 h-5 rounded-full"
+              src={user.picture}
+              alt="avatar"
+              onError={(e) => {
+                console.log("IMAGE ERROR");
+                e.currentTarget.src = defaultAvatar;
+              }}
+            />
+          ) : (
+            <div className="w-5 h-5 rounded-full bg-neutral-600" />
+          )}
         </div>
         <span className="text-sm font-medium">{user?.name}</span>
       </NavLink>
